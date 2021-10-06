e27fd4e6f7d4cab4574fefeec84de49f516c6723
bartosz12-12 <73654110+bartosz12-12@users.noreply.github.com>
2021-10-06 12:42:47 +0200

    Add files via upload

---
+++ PO-gr3-BartoszLip/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+//zad1
+    public static void main(String[] args) {
+
+        int styczen=31;
+        int luty=29;
+        int marzec=31;
+        System.out.println(styczen+luty+marzec);
+
+        //zad2
+        int suma=1;
+         for(int i=1;i<=10;i++)
+            suma+=i;
+
+        System.out.println(suma);
+
+        //zad3
+        int iloczyn=1;
+        for(int i=1;i<=10;i++)
+            iloczyn*=i;
+
+        System.out.println(iloczyn);
+
+        //zad4
+
+        //zad5
+
+        //zad6
+        System.out.println("  ///// \n +\"\"\"\"\"+ \n(| o o |) \n  | ^ | \n |,_ , |");
+        //zad7
+
+        //zad8
+
+
+    }
+}
+
