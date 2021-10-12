e963adde802e752bffc0120bd0cb521c7352d4a4
bartosz12-12 <73654110+bartosz12-12@users.noreply.github.com>
2021-10-12 14:18:51 +0200

    Add files via upload

---
+++ BartoszLip/src/Main.java
@@ -0,0 +1,93 @@
+public class Main {
+    //zad1
+    public static void main(String[] args) {
+
+//        int styczen=31;
+//        int luty=29;
+//        int marzec=31;
+//        System.out.println(styczen+luty+marzec);
+//
+//        //zad2
+//        int suma=1;
+//        for(int i=1;i<=10;i++)
+//            suma+=i;
+//
+//        System.out.println(suma);
+//
+//        //zad3
+//        int iloczyn=1;
+//        for(int i=1;i<=10;i++)
+//            iloczyn*=i;
+//
+//        System.out.println(iloczyn);
+//
+//        //zad4
+//        float stanKonta = 1000;
+//        for (int i=1;i<=3;i++){
+//            stanKonta+=stanKonta*0.06;
+//            System.out.println("Saldo po "+ i +" wynosi=" + stanKonta);
+//        }
+//        //zad5
+//        System.out.println("_______\n|JAVA|\n-------");
+//        //zad6
+//        System.out.println("  ///// \n +\"\"\"\"\"+ \n(| o o |) \n  | ^ | \n |,_ , |");
+//        //zad7
+//        System.out.println("" +
+//                "* * *                                        *                                     \n" +
+//                "*     *                                      *              *                      \n" +
+//                "*    *                                       *                                     \n" +
+//                "* * * *        * * *       *     * *         *              *     * * * * * *      \n" +
+//                "*      *     *       *       * *     *       *              *     *         *      \n" +
+//                "*       *   *         *       *              *              *     *         *      \n" +
+//                "*      *     *       * *      *              *              *     *         *      \n" +
+//                "* * * *        * * *     *    *              * * * * * *    *     * * * * * *      \n" +
+//                "                                                                  *                \n" +
+//                "                                                                  *                \n" +
+//                "                                                                  *                \n" +
+//                "                                                                  *                   ");
+//
+
+//        //zad8
+//        System.out.println("    +    \n" +
+//                "   + +   \n  +   +  \n" +
+//                " +-----+ \n" +
+//                " | .-. | \n | | | | \n" +
+//                " +-+-+-+ ");
+          //zad9
+//        System.out.println("" +
+//                "                     _________   \n" +
+//                "                    |   Witaj | \n" +
+//                "                    |____  ___|\n" +
+//                "                        / /\n" +
+//                "   (-)   /\\ /\\   (-)  <=_/\n" +
+//                "     \\\\ (* ^ *) //\n" +
+//                "      \\\\ ~~~~~ //\n" +
+//                "        |~~~~~|\n" +
+//                "        \\~~~~~/\n" +
+//                "         | | |\n" +
+//                "         | | |\n" +
+//                "        (__|__)");
+        //zad10
+//        System.out.println("1.Roki\n2.Poprostu walcz\n3.Nietykalni\n\n");
+        //zad11
+//        System.out.println("Dwadzieścia siedem kości,\n" +
+//                "trzydzieści pięć mięśni,\n" +
+//                "około dwóch tysięcy komórek nerwowych\n" +
+//                "w każdej opuszce naszych pięciu palców.\n" +
+//                "To zupełnie wystarczy,\n" +
+//                "żeby napisać „Mein Kampf”\n" +
+//                "albo „Chatkę Puchatka”.\n\n");
+
+            //zad12
+        System.out.println("* * * * * *=======================================\n" +
+                " * * * * *========================================\n" +
+                "* * * * * *=======================================\n" +
+                " * * * * *========================================\n" +
+                "* * * * * *=======================================\n" +
+                "==================================================\n" +
+                "==================================================\n" +
+                "==================================================\n" +
+                "==================================================\n" +
+                "==================================================");
+    }
+}
