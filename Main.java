74ab3128502475439927787ac093b5e6874e9941
bartosz12-12 <73654110+bartosz12-12@users.noreply.github.com>
2021-10-13 11:32:57 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+
+public class Main {
+//zad1
+
+
+    public static void main(String[] args) {
+//        double wynik=0;
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        for(int i=0;i<=n-1;i++) {
+//            double a = scan.nextDouble();
+//            wynik += a;
+//        }
+//      System.out.println(wynik);
+
+//        double wynik=1;
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        for(int i=0;i<=n-1;i++) {
+//            double a = scan.nextDouble();
+//            wynik *= a;
+//        }
+//        System.out.println(wynik);
+
+
+//        double wynik=1;
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        for(int i=0;i<=n-1;i++) {
+//            double a = scan.nextDouble();
+//            wynik +=abs(a);
+//        }
+//        System.out.println(wynik);
+
+//        double wynik=1;
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        for(int i=0;i<=n-1;i++) {
+//            double a = scan.nextDouble();
+//            wynik +=sqrt(a);
+//        }
+//        System.out.println(wynik);
+
+
+//        double wynik=1;
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        for(int i=0;i<=n-1;i++) {
+//            double a = scan.nextDouble();
+//            wynik *=pow(a,2);
+//        }
+//        System.out.println(wynik);
+
+
+
+
+
+    }
+}
+
