e009a435fef786e161b8e6c7bf7d944c54f5cf21
bartosz12-12 <73654110+bartosz12-12@users.noreply.github.com>
2021-12-01 05:17:30 +0100

    Add files via upload

---
+++ lab7/src/pl/imiajd/lipinski/Punkt.java
@@ -0,0 +1,26 @@
+package pl.imiajd.lipinski;
+
+public class Punkt {
+
+    private int x;
+    private int y;
+
+    public Punkt(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void show(){
+        System.out.println("<" + x  + ", " + y + ">");
+
+
+    }
+}
